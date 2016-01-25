9e126652551b4db990fe56b3bf28486ecd02a118
Hittesh Ahuja <ha386@bath.ac.uk>
2016-01-25 15:41:24 +0000

    Get deleted users in Moodle database

---
+++ moodle/housekeeping/moodle_deleted_users.sql
@@ -0,0 +1,11 @@
+/* Get deleted users in Moodle database */
+SELECT
+             u.id AS 'USERID' ,u.firstname,
+  u.lastname,u.username AS 'BUCS Username' ,u.email,u.deleted
+FROM
+  mdl5_user u
+WHERE
+  u.deleted = 1 -- user is deleted
+  AND email <> '' -- email is not blank*/
+  AND (username <> '' AND username NOT LIKE 'ltiprovider%' AND (firstname NOT LIKE '%test%' OR lastname NOT LIKE '%test%'))
+;
