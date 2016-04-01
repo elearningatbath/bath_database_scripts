5ad52e6f62939228d5d7952e38301013f744f020
Hittesh Ahuja <ha386@bath.ac.uk>
2016-04-01 16:45:13 +0100

    Added comment

--- moodle/panopto/block_instances.sql
+++ moodle/panopto/block_instances.sql
@@ -4,5 +4,5 @@ JOIN mdl5_context ctx
             ON (ctx.id = bi.parentcontextid)
       JOIN mdl5_course c ON c.id = ctx.instanceid AND ctx.contextlevel = 50
       JOIN mdl5_course_categories cat on cat.id = c.category
-      WHERE bi.blockname = 'panopto' AND cat.id IN (13,35,36,2,37,39,31,32)
+      WHERE bi.blockname = 'panopto' AND cat.id IN (13,35,36,2,37,39,31,32) -- Change this as required
 GROUP BY cat.id;
