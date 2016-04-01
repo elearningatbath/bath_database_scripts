8727b271caee22d2353dec5cb4e8348905677884
Hittesh Ahuja <ha386@bath.ac.uk>
2016-04-01 16:43:11 +0100

    Changed it a bit

--- moodle/Statistics/no_of_views.sql
+++ moodle/Statistics/no_of_views.sql
@@ -1,5 +1,8 @@
-SELECT c.id,COALESCE(count(l.id),0) AS 'No of Views'
+-- number of course views for all courses in a certain category
+SELECT c.id,COUNT(DISTINCT (l.id)) AS 'No of Course Views'
 FROM mdl5_logstore_standard_log l
-RIGHT JOIN mdl5_course c ON c.id = l.courseid  AND l.crud = 'r'
+ RIGHT JOIN mdl5_course c ON (c.id = l.courseid)
+                             AND  l.action = 'viewed' AND l.target ='course' AND l.eventname = '\\core\\event\\course_viewed'
+                             AND l.timecreated BETWEEN 1441062000 AND 1454198400 AND l.edulevel  = 2
  JOIN mdl5_course_categories cat ON c.category = cat.id AND  cat.id IN (14)
 GROUP BY c.id;
